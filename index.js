50a6ec1492275f919bc2ff57a54333bc75b601b1
Samad191 <samad13354@gmail.com>
2021-01-26 19:40:04 +0500

    add build

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static("./build"));
+app.all("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/build/app/index.html"));
+});
+
+app.listen(process.env.PORT || 3000, (_) =>
+  console.log("started at 3000 port frontend on heroku")
+);
